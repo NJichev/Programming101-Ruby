4998a3f1e5190cb88fceb74a610c99ea1bc4ed4a
niko <njichev@gmail.com>
2016-03-12 12:05:50 +0200

    Some changes.

---
+++ week08/2-meta/solution.rb
@@ -0,0 +1,30 @@
+class Object
+  def singleton_class
+    class << self
+      self
+    end
+  end
+
+  def define_singleton_method(name, method = nil, &block)
+    if method.nil?
+      self.class.send(:define_method, name, method)
+    else
+      self.class.send(:define_method, name, &block)
+    end
+  end
+end
+
+class String
+  def to_proc
+    proc do |obj, args|
+      self.split('.').each do |m|
+        obj.send(m, *args)
+      end
+    end
+  end
+end
+
+p [2, 3, 4, 5].map(&'succ.succ')
+
+
+
