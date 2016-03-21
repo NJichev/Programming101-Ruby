09d31822489a0d462c306672c9f801560f906a94
niko <njichev@gmail.com>
2016-03-21 14:17:04 +0200

    Add my solutions to week03 from old folder.

--- week03/1-fmi-tribute/patch_array.rb
+++ week03/1-fmi-tribute/patch_array.rb
@@ -29,3 +29,38 @@ class Array
     end
   end
 end
+
+#
+#
+#
+# class Array
+#   def to_hash
+#     Hash.new.tap do |h|
+#       each { |x| h[x[0]] = x[1] }
+#     end
+#   end
+# 
+#   def index_by
+#     Hash.new.tap do |h|
+#       each do |x|
+#         h[yield x] = x
+#       end
+#     end
+#   end
+# 
+#   def subarray_count(sub_arr)
+#     # lame
+#     index, count = 0, 0
+#     while index < length - 1
+#       count += 1 if self[index..index + sub_arr.length - 1] == sub_arr
+#       index += 1
+#     end
+#     count
+#   end
+# 
+#   def occurences_count
+#     Hash.new(0).tap do |hash|
+#       each { |x| hash[x] += 1 }
+#     end
+#   end
+# end
